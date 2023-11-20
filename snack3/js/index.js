e15ec6389f03e78f443c541f1be1dfbb150fbded
AlfredoCastrogiovanni <castro.alfry@gmail.com>
2023-11-20 11:48:14 +0100

    snack3: esercizio completato

---
+++ snack3/js/index.js
@@ -0,0 +1,15 @@
+
+function wordLength (firstWord, secondWord) {
+    if (firstWord.length > secondWord.length) {
+        console.log(firstWord);
+    } else if (firstWord.length < secondWord.lenght) {
+        console.log(secondWord);
+    } else {
+        console.log(firstWord, secondWord);
+    }
+}
+
+const firstWord = prompt("Inserisci la prima parola: ");
+const secondWord = prompt("Inserisci la seconda parola: ");
+firstWord.length
+wordLength(firstWord, secondWord);
