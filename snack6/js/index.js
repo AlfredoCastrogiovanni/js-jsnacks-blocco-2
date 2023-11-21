1c45af46e142b5801c9f7ccc7b5c8335913c0825
AlfredoCastrogiovanni <castro.alfry@gmail.com>
2023-11-21 12:36:20 +0100

    snack6: pulizia codice e creazione di un altra funzione

--- snack6/js/index.js
+++ snack6/js/index.js
@@ -1,28 +1,37 @@
 
 const wrapper = document.getElementById("wrapper");
 const userSquareNumber = parseInt(prompt("Inserisci il numero di quadrati: "));
-createSquare(userSquareNumber);
+createSquareTimes(userSquareNumber);
 
-function createSquare(squareNumber) {
-    for(let i=0; i < squareNumber; i++) {
-        const newSquare = document.createElement("div");
-        newSquare.classList.add("square");
 
-        const squareContent = document.createElement("span");
-        squareContent.textContent = generateRandomNumber(1,i);
-        newSquare.append(squareContent);
-        
-        parseInt(squareContent.textContent) % 2 == 0 ? newSquare.classList.add("bg-blue") : newSquare.classList.add("bg-red");
-
-        newSquare.addEventListener("click", function() {
-            this.classList.toggle("clicked");
-        });
-        
-        wrapper.append(newSquare);
-    }
-}
+// ==================== FUNCTIONS ====================
 
 // Random number between two numbers
 function generateRandomNumber(min, max) {
     return Math.floor((Math.random() * (max - min)) + min);
+}
+
+// create new square many times
+function createSquareTimes(SquareNumber) {
+    for(let i=0; i < squareNumber; i++) {
+        createSquare();
+    }
+}
+
+// return a new square
+function createSquare() {
+    const newSquare = document.createElement("div");
+    newSquare.classList.add("square");
+
+    const squareContent = document.createElement("span");
+    squareContent.textContent = generateRandomNumber(1,i);
+    newSquare.append(squareContent);
+    
+    parseInt(squareContent.textContent) % 2 == 0 ? newSquare.classList.add("bg-blue") : newSquare.classList.add("bg-red");
+
+    newSquare.addEventListener("click", function() {
+    this.classList.toggle("clicked");
+    });
+    
+    wrapper.append(newSquare);
 }
