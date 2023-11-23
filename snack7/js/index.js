942222314098d665b5b0bc065b07ddd1e5286f9d
AlfredoCastrogiovanni <castro.alfry@gmail.com>
2023-11-23 12:27:04 +0100

    snack7: implementazione timer stop

--- snack7/js/index.js
+++ snack7/js/index.js
@@ -3,13 +3,14 @@ const output = document.getElementById("output");
 
 const buttonStart = document.getElementById("start");
 const buttonStop = document.getElementById("stop");
+let myInterval;
 
 buttonStart.addEventListener("click", () => {
-    setInterval(() => {
+    myInterval = setInterval(() => {
     output.innerText = parseInt(output.innerText, 10) + 1
     }, 1000);
 });
 
 buttonStop.addEventListener("click", () => {
-    clearInterval();
+    clearInterval(myInterval);
 });
